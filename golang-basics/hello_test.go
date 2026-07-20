96655b6999705c7b8e13e89372a49e0f4fcc4b48
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-07-20 18:20:27 +0300

    feat: refactor test suite assertion into new function.

--- golang-basics/hello_test.go
+++ golang-basics/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("an empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
-    got := Hello("Chris")
-	want := "Hello, Chris"
-
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
