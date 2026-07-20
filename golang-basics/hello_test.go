57223b024a857e5d9725f8a8d3e9499eaacdceda
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-07-20 19:28:43 +0300

    feat: add language parameter.

--- golang-basics/hello_test.go
+++ golang-basics/hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("an empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Rodrigo", "Spanish")
+		want := "Hola, Rodrigo"
 		assertCorrectMessage(t, got, want)
 	})
 }
