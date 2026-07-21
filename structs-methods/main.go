0ed2b2f4111d016f6242dae42552d3776eaffb5d
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-07-21 23:20:36 +0300

    feat: structs,methods and interfaces.

--- structs-methods/main.go
+++ structs-methods/main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"math"
+)
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	area := rectangle.Width * rectangle.Height
+func (c Circle) Area() float64 {
+	area := c.Radius * c.Radius * math.Pi
+	return area
+}
+
+func (r Rectangle) Area() float64 {
+	area := r.Width * r.Height
 	return area
 }
--- structs-methods/main_test.go
+++ structs-methods/main_test.go
@@ -15,11 +15,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{7.0, 10.0}
-	got := Area(rectangle)
-	want := 70.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	checkArea := func
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{7.0, 10.0}
+		got := rectangle.Area()
+		want := 70.0
+	
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
